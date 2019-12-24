ce3e39a9f2cbab97a945750f823ebfe2733b6c57
dohq <dorastone@gmail.com>
2019-12-24 22:58:17 +0900

    [mod] return os.Remove error and os.Exit

--- langserver/exit.go
+++ langserver/exit.go
@@ -2,11 +2,17 @@ package langserver
 
 import (
 	"context"
-	lsp "github.com/sourcegraph/go-lsp"
 	"os"
+
+	lsp "github.com/sourcegraph/go-lsp"
 )
 
 func Exit(ctx context.Context, vs lsp.None) error {
-	os.Remove(tempFile.Name())
+	err := os.Remove(tempFile.Name())
+	if err != nil {
+		return err
+	}
+
+	os.Exit(0)
 	return nil
 }
