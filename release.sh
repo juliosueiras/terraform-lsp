480b2d4483ae74f39ae1f5a21d8700909b1c216d
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-04-21 13:26:03 -0400

    Updated release.sh for testing

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@ docker run --rm --privileged \
   -v $PWD:/go/src/github.com/juliosueiras/terraform-lsp \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -w /go/src/github.com/juliosueiras/terraform-lsp \
-  mailchain/goreleaser-xcgo goreleaser --rm-dist
+  mailchain/goreleaser-xcgo release --rm-dist
