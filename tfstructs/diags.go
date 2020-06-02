424c020f787d17d9118018533e7ffe43aff44746
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-06-02 12:10:31 +0100

    Lint : diags cannot be nil as a non-pointer value

--- tfstructs/diags.go
+++ tfstructs/diags.go
@@ -214,24 +214,22 @@ func GetDiagnostics(fileName string, originalFile string) []lsp.Diagnostic {
 	for _, local := range cfg.Locals {
 		diags := GetLocalsForDiags(*local, filepath.Dir(originalFileName), variables)
 
-		if diags != nil {
-			for _, diag := range diags {
-				result = append(result, lsp.Diagnostic{
-					Severity: lsp.DiagnosticSeverity(diag.Severity),
-					Message:  diag.Detail,
-					Range: lsp.Range{
-						Start: lsp.Position{
-							Line:      diag.Subject.Start.Line - 1,
-							Character: diag.Subject.Start.Column - 1,
-						},
-						End: lsp.Position{
-							Line:      diag.Subject.End.Line - 1,
-							Character: diag.Subject.End.Column - 1,
-						},
+		for _, diag := range diags {
+			result = append(result, lsp.Diagnostic{
+				Severity: lsp.DiagnosticSeverity(diag.Severity),
+				Message:  diag.Detail,
+				Range: lsp.Range{
+					Start: lsp.Position{
+						Line:      diag.Subject.Start.Line - 1,
+						Character: diag.Subject.Start.Column - 1,
 					},
-					Source: "Terraform Schema",
-				})
-			}
+					End: lsp.Position{
+						Line:      diag.Subject.End.Line - 1,
+						Character: diag.Subject.End.Column - 1,
+					},
+				},
+				Source: "Terraform Schema",
+			})
 		}
 	}
 
